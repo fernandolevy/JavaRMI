790a47784a2f398f2ea710276929422adc33ce96
fernandolevy <fernandoassis@alunos.utfpr.edu.br>
2019-10-25 14:33:25 -0300

    comentado tudo

--- Client/src/VagaDeEmpregoServer/VagaDeEmpregoServant.java
+++ Client/src/VagaDeEmpregoServer/VagaDeEmpregoServant.java
@@ -10,10 +10,14 @@ import java.rmi.server.UnicastRemoteObject;
 
 //Esta classe e responsavel por implementar e servir os metodos decladaros na VagaDeEmpregoInterface
 public class VagaDeEmpregoServant extends UnicastRemoteObject implements VagaDeEmpregoInterface {
+    //Vetor global com as vagas de emprego
     private Vector theList;
+    //Vetor global com as areas de interesse
     private Vector interesseList;
+    //Vetor global com a referencia dos clientes que registraram interesse em vagas de emprego
     private Vector interesseCallbackList;
 
+    //Inicializa vetor
     public VagaDeEmpregoServant() throws RemoteException {
         theList = new Vector();
         interesseList = new Vector();
@@ -22,11 +26,16 @@ public class VagaDeEmpregoServant extends UnicastRemoteObject implements VagaDeE
 
 
     public void verificaInteresseVagaDeEmprego(VagaDeEmpregoObject v) throws RemoteException {
+        //Percorre vetor de objetos vaga de emprego e verifica se casa com a area de interesse registrada pelo metodo registraInteresseVagaDeEmprego
         VagaDeEmpregoInterfaceCallback notify = new VagaDeEmpregoServantCallback();
         for (int i = 0; i < interesseList.size(); i++) {
+            //salva area de interesse registrada no vetor
             String g = (String) interesseList.elementAt(i);
+            //Salva a interface do callback salvo no registro de interesse
             VagaDeEmpregoInterfaceCallback c = (VagaDeEmpregoInterfaceCallback) interesseCallbackList.elementAt(i);
+            //Caso a area de interesse registrada case com area de interesse cadastrada da Vaga de Emprego
             if (v.area_da_vaga.equals(g)) {
+                //Notifica cliente com a vaga de emprego de seu interesse
                 c.notifyVagaDeEmprego(v);
             }
         }
@@ -41,31 +50,40 @@ public class VagaDeEmpregoServant extends UnicastRemoteObject implements VagaDeE
         System.out.println("        cnpj:" + v.cnpj);
         System.out.println("        contato_empresa:" + v.contato_empresa);
         System.out.println("|---------------------------------------------------|\n\n");
+        //Salva na lista global o curriculo cadastrado
         theList.addElement(v);
+        //Verifica se a area de interesse da Vagas De Emprego cadastrada casa com uma das aread de interesse contidas no vetor de Interesses registrados
         verificaInteresseVagaDeEmprego(v);
     }
 
     public Vector consultarVagaDeEmprego(String area_da_vaga) throws RemoteException {
+        //Percorre vetor de objetos Vagas de emprego ate encontrar objeto com a area da vaga de emprego enviado pelo Candidato
         Vector vagas = new Vector();
         for (int i = 0; i < theList.size(); i++) {
             VagaDeEmpregoObject g = (VagaDeEmpregoObject) theList.elementAt(i);
             if (area_da_vaga.equals(g.area_da_vaga)) {
+                //Salvar em um vetor de objetos vaga de emprego
                 vagas.addElement(g);
             }
         }
+        //retorna vaga de emprego
         return vagas;
     }
 
     public Vector getVagasDeEmprego() throws RemoteException {
+        //retorna Vector global com as Vagas De Emprego
         return theList;
     }
 
     public void registraInteresseVagaDeEmprego(VagaDeEmpregoInterfaceCallback callback, String area_de_interesse) throws RemoteException {
+        //Salva o interesse em um vetor de strings
         interesseList.addElement(area_de_interesse);
+        //Salva o callback em um vetor das Interfaces do callback
         interesseCallbackList.addElement(callback);
     }
 
     public boolean alterarVagaDeEmprego(String cnpj, VagaDeEmpregoObject v) throws RemoteException {
+        //Atualiza vaga de emprego alterada apartir do cnpj e percorre o vetor de objetos das vagas de emprego
         for (int i = 0; i < theList.size(); i++) {
             VagaDeEmpregoObject g = (VagaDeEmpregoObject) theList.elementAt(i);
             if (g.cnpj.equals(cnpj)) {
@@ -77,6 +95,7 @@ public class VagaDeEmpregoServant extends UnicastRemoteObject implements VagaDeE
                 System.out.println("        cnpj:" + v.cnpj);
                 System.out.println("        contato_empresa:" + v.contato_empresa);
                 System.out.println("|---------------------------------------------------|\n\n");
+                //Atualiza no vetor global
                 theList.set(i, v);
                 return true;
             }
--- Server/src/CurriculoServer/CurriculoServant.java
+++ Server/src/CurriculoServer/CurriculoServant.java
@@ -8,11 +8,14 @@ import java.util.Vector;
 
 //Esta classe e responsavel por implementar e servir os metodos decladaros na CurriculoInterface
 public class CurriculoServant extends UnicastRemoteObject implements CurriculoInterface {
+    //Vetor global com os curriculos
     private Vector theList;
+    //Vetor global com as areas de interesse
     private Vector interesseList;
+    //Vetor global com a referencia dos clientes que registraram interesse em curriculos
     private Vector interesseCallbackList;
 
-
+    //Inicializa vetor
     public CurriculoServant() throws RemoteException {
         theList = new Vector();
         interesseList = new Vector();
@@ -28,15 +31,22 @@ public class CurriculoServant extends UnicastRemoteObject implements CurriculoIn
         System.out.println("        cpf: " + v.cpf);
         System.out.println("        contato_candidato: " + v.contato_candidato);
         System.out.println("|---------------------------------------------------|\n\n");
+        //Salva objeto curriculo no vetor global da classe
         theList.addElement(v);
+        //Verifica se curriculo cadastrado casa com uma dos curriculos de interesse contido no vetor de Interesses registrados
         verificaInteresseCurriculo(v);
     }
 
     public void verificaInteresseCurriculo(CurriculoObject v) throws RemoteException {
+        //Percorre vetor de objetos curriculos e verifica se casa com a area de interesse registrada pelo metodo registraInteresseCurriculo
         for (int i = 0; i < interesseList.size(); i++) {
+            //salva area de interesse registrada no vetor
             String g = (String) interesseList.elementAt(i);
+            //Salva a interface do callback salvo no registro de interesse
             CurriculoInterfaceCallback c = (CurriculoInterfaceCallback) interesseCallbackList.elementAt(i);
+            //Caso a area de interesse registrada case com area de interesse cadastrada do Curriculo
             if (v.area_de_interesse.equals(g)) {
+                //Notifica cliente com o curriculo de seu interesse
                 c.notifyCurriculo(v);
             }
         }
@@ -44,27 +54,34 @@ public class CurriculoServant extends UnicastRemoteObject implements CurriculoIn
 
     public Vector consultarCurriculo(String area_de_interesse, String salario_pretendido) throws RemoteException {
         Vector curriculos = new Vector();
+        //Percorre vetor de objetos curriculos ate encontrar objeto com a area de interesse e salario pretendido enviado pela Empresa
         for (int i = 0; i < theList.size(); i++) {
             CurriculoObject g = (CurriculoObject) theList.elementAt(i);
             if (area_de_interesse.equals(g.area_de_interesse) && (salario_pretendido.equals(g.salario_pretendido))) {
+                //Salvar em um vetor de objetos curriculos
                 curriculos.addElement(g);
             }
         }
+        //Retorna curriculo encontrado
         return curriculos;
     }
 
 
     public Vector getCurriculos() throws RemoteException {
+        //retorna Vector global com os curriculos
         return theList;
     }
 
 
     public void registraInteresseCurriculo(CurriculoInterfaceCallback callback, String area_de_interesse) throws RemoteException {
+        //Salva o interesse em um vetor de strings
         interesseList.addElement(area_de_interesse);
+        //Salva o callback em um vetor das Interfaces do callback
         interesseCallbackList.addElement(callback);
     }
 
     public boolean alterarCurriculo(String cpf, CurriculoObject v) throws RemoteException {
+        //Atualiza vaga de emprego alterada apartir do cpf e percorre o vetor de objetos dos Curriculos
         for (int i = 0; i < theList.size(); i++) {
             CurriculoObject g = (CurriculoObject) theList.elementAt(i);
             if (g.cpf.equals(cpf)) {
@@ -76,6 +93,7 @@ public class CurriculoServant extends UnicastRemoteObject implements CurriculoIn
                 System.out.println("        cpf: " + v.cpf);
                 System.out.println("        contato_candidato: " + v.contato_candidato);
                 System.out.println("|---------------------------------------------------|\n\n");
+                //Atualiza o curriculo no vetor global
                 theList.set(i, v);
                 return true;
             }
